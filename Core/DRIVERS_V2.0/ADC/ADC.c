c3b9764fb8b7eb4a0f35217237e9045f4937f054
Bavin <bavinbav5@gmail.com>
2026-07-21 22:44:40 +0300

    adc init function is complete

--- Core/DRIVERS_V2.0/ADC/ADC.c
+++ Core/DRIVERS_V2.0/ADC/ADC.c
@@ -6,8 +6,19 @@ static inline void adc_on(ADC_TypeDef *adc){
 	else if(adc==ADC3) RCC->APB2ENR|=(1<<10);
 }
 
-void ADCx_init(ADC_TypeDef *adc,GPIO_TypeDef *port,uint8_t pin){
-	adc_on(adc);
+static inline void channel_adc(ADC_TypeDef *adc_port,uint8_t chn){
+	if(chn>=0&&chn<=9){
+		adc_port->SMPR2&=~((0b111<<(chn*3)));
+		adc_port->SMPR2|=(0b110<<(chn*3));				//sample time is hardcoded to 144
+
+	}
+	else if(chn>=10&&chn<=15){
+		adc_port->SMPR1&=~((0b111<<(chn*3)));
+		adc_port->SMPR1|=(0b110<<(chn*3));
+	}
+}
+
+void ADCx_init(ADC_TypeDef *adc,GPIO_TypeDef *port,uint8_t pin,uint8_t chn){
 	gpio_config_t gpio_setup={
 		.pin=pin,
 		.mode=GPIOx_MODE_ANALOG,
@@ -15,6 +26,11 @@ void ADCx_init(ADC_TypeDef *adc,GPIO_TypeDef *port,uint8_t pin){
 	};
 
 	GPIO_init(port, &gpio_setup);
+	adc_on(adc);
+
+	adc->CR2|=(1<<0);				//EN ADC
+
+	channel_adc(adc, chn);
 }
 
 void ADCx_chn_config(ADC_TypeDef *adc,uint8_t channel,uint8_t rank){
--- Core/DRIVERS_V2.0/ADC/ADC.h
+++ Core/DRIVERS_V2.0/ADC/ADC.h
@@ -5,7 +5,7 @@
 #include "stm32f446xx.h"
 #include "GPIO.h"
 
-void ADCx_init(ADC_TypeDef *adc,GPIO_TypeDef *port,uint8_t pin);
+void ADCx_init(ADC_TypeDef *adc,GPIO_TypeDef *port,uint8_t pin,uint8_t chn);
 void ADCx_chn_config(ADC_TypeDef *adc,uint8_t channel,uint8_t rank);
 int16_t ADCx_read(ADC_TypeDef *adc,uint8_t channel);
 
