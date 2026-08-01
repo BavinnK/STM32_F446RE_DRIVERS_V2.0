b8dde45f64b30f7f235a416b2e9abc4999bd1fe9
Bavin <bavinbav5@gmail.com>
2026-08-01 12:18:02 +0300

    i think TIM IC init is done now i have to test if its working

--- Core/DRIVERS_V2.0/TIM/TIM_IC.c
+++ Core/DRIVERS_V2.0/TIM/TIM_IC.c
@@ -4,14 +4,24 @@
 //INLINE FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-inline static uint32_t pick_ccmr(TIM_TypeDef *tim, uint8_t chn){
+inline static  volatile uint32_t* pick_ccmr(TIM_TypeDef *tim, uint8_t chn){
 	if(chn==1 || chn==2) 		return &tim->CCMR1;
 	else if(chn==3 || chn==4) 	return &tim->CCMR2;
+	else return (void*) 0;
 }
 
 inline static uint8_t ccmr_shift(uint8_t chn){
 	if(chn==1 || chn==3) 		return 0;
 	else if(chn==2 || chn==4) 	return 8;
+	else while(1);
+}
+
+inline static uint8_t ccer(TIM_TypeDef *tim,uint8_t chn){
+	if(chn==1) return 0;
+	else if(chn==2) return 4;
+	else if(chn==3) return 8;
+	else if(chn==4) return 12;
+	else while(1);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,8 +46,19 @@ void TIMx_IC_init(TIM_TypeDef *tim,tim_ic_config_t *config, uint8_t channel){
 	tim->CR1|=(config->center_align_mode<<5);
 
 	volatile uint32_t *CCMRx=pick_ccmr(tim, channel);
-	uint8_t shift=ccmr_shift(channel);
+	uint8_t shift_ccmr=ccmr_shift(channel);
 
+	*CCMRx&=~(((3<<0)|(3<<2)|(0b1111<<4))<<shift_ccmr);
+	*CCMRx|=((1<<0)|(config->input_capture_pcs<<2)|(config->input_capture_filter<<4))<<shift_ccmr;
+
+	uint8_t shift_ccer=ccer(tim, channel);
+
+	uint8_t polarity_bits=0;
+	if(polarity_bits & 0b01) polarity_bits|=(1<<1);
+	if(polarity_bits & 0b10) polarity_bits|=(1<<3);
+
+	tim->CCER&=~(((1<<0)|(1<<1)|(1<<3))<<shift_ccer);
+	tim->CCER|=((1<<0)|polarity_bits)<<shift_ccer;
 
 }
 
