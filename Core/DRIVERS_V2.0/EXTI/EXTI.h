fc0ffe0e80f771a71e6c6e749cbec2c913c4f52c
Bavin <bavinbav5@gmail.com>
2026-08-04 22:08:46 +0300

    now its time for EXTI driver

---
+++ Core/DRIVERS_V2.0/EXTI/EXTI.h
@@ -0,0 +1,7 @@
+#ifndef EXTI_H
+#define EXTI_H
+
+#include "stdint.h"
+#include "stm32f446xx.h"
+
+#endif
--- Debug/Core/DRIVERS_V2.0/EXTI/subdir.mk
+++ Debug/Core/DRIVERS_V2.0/EXTI/subdir.mk
@@ -5,23 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/DRIVERS_V2.0/UART/UART.c 
+../Core/DRIVERS_V2.0/EXTI/EXTI.c 
 
 OBJS += \
-./Core/DRIVERS_V2.0/UART/UART.o 
+./Core/DRIVERS_V2.0/EXTI/EXTI.o 
 
 C_DEPS += \
-./Core/DRIVERS_V2.0/UART/UART.d 
+./Core/DRIVERS_V2.0/EXTI/EXTI.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/DRIVERS_V2.0/UART/%.o Core/DRIVERS_V2.0/UART/%.su Core/DRIVERS_V2.0/UART/%.cyclo: ../Core/DRIVERS_V2.0/UART/%.c Core/DRIVERS_V2.0/UART/subdir.mk
+Core/DRIVERS_V2.0/EXTI/%.o Core/DRIVERS_V2.0/EXTI/%.su Core/DRIVERS_V2.0/EXTI/%.cyclo: ../Core/DRIVERS_V2.0/EXTI/%.c Core/DRIVERS_V2.0/EXTI/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/EXTI" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/TIM" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/ADC" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/DMA" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/UART" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/DELAY" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/GPIO" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/CLOCK" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
-clean: clean-Core-2f-DRIVERS_V2-2e-0-2f-UART
+clean: clean-Core-2f-DRIVERS_V2-2e-0-2f-EXTI
 
-clean-Core-2f-DRIVERS_V2-2e-0-2f-UART:
-	-$(RM) ./Core/DRIVERS_V2.0/UART/UART.cyclo ./Core/DRIVERS_V2.0/UART/UART.d ./Core/DRIVERS_V2.0/UART/UART.o ./Core/DRIVERS_V2.0/UART/UART.su
+clean-Core-2f-DRIVERS_V2-2e-0-2f-EXTI:
+	-$(RM) ./Core/DRIVERS_V2.0/EXTI/EXTI.cyclo ./Core/DRIVERS_V2.0/EXTI/EXTI.d ./Core/DRIVERS_V2.0/EXTI/EXTI.o ./Core/DRIVERS_V2.0/EXTI/EXTI.su
 
-.PHONY: clean-Core-2f-DRIVERS_V2-2e-0-2f-UART
+.PHONY: clean-Core-2f-DRIVERS_V2-2e-0-2f-EXTI
 
