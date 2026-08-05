2afe2e2011db79d6041b6b749342c51bbe48f994
Bavin <bavinbav5@gmail.com>
2026-08-05 21:05:32 +0300

    now all the handlers are ready, and the driver is ready to be tested

--- Core/DRIVERS_V2.0/EXTI/EXTI.c
+++ Core/DRIVERS_V2.0/EXTI/EXTI.c
@@ -180,12 +180,91 @@ void EXTI9_5_IRQHandler(void){
 			exti_callback_5();
 		}
 	}
+	if(EXTI->PR&(1<<6)){
+		EXTI->PR=(1<<6);
+
+		if(exti_callback_6 != NULL){
+			exti_callback_6();
+		}
+	}
+
+	if(EXTI->PR&(1<<7)){
+		EXTI->PR=(1<<7);
+
+		if(exti_callback_7 != NULL){
+			exti_callback_7();
+		}
+	}
+
+	if(EXTI->PR&(1<<8)){
+		EXTI->PR=(1<<8);
+
+		if(exti_callback_8 != NULL){
+			exti_callback_8();
+		}
+	}
+
+	if(EXTI->PR&(1<<9)){
+		EXTI->PR=(1<<9);
+
+		if(exti_callback_9 != NULL){
+			exti_callback_9();
+		}
+	}
 }
 
 
 
-void EXTI15_10_IRQHandler(void){
 
+
+void EXTI15_10_IRQHandler(void){
+	if(EXTI->PR&(1<<10)){
+		EXTI->PR=(1<<10);
+
+		if(exti_callback_10 != NULL){
+			exti_callback_10();
+		}
+	}
+
+	if(EXTI->PR&(1<<11)){
+		EXTI->PR=(1<<11);
+
+		if(exti_callback_11 != NULL){
+			exti_callback_11();
+		}
+	}
+
+	if(EXTI->PR&(1<<12)){
+		EXTI->PR=(1<<12);
+
+		if(exti_callback_12 != NULL){
+			exti_callback_12();
+		}
+	}
+
+	if(EXTI->PR&(1<<13)){
+		EXTI->PR=(1<<13);
+
+		if(exti_callback_13 != NULL){
+			exti_callback_13();
+		}
+	}
+
+	if(EXTI->PR&(1<<14)){
+		EXTI->PR=(1<<14);
+
+		if(exti_callback_14 != NULL){
+			exti_callback_14();
+		}
+	}
+
+	if(EXTI->PR&(1<<15)){
+		EXTI->PR=(1<<15);
+
+		if(exti_callback_15 != NULL){
+			exti_callback_15();
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
