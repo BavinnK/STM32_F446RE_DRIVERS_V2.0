a08ba64f27ee25968dd332fd5d02a57f8215bfc5
Bavin <bavinbav5@gmail.com>
2026-08-16 15:25:30 +0300

    SPI POLLING driver is ready, and i iwll not test it until all 3 drivers, POLLING, DMA, INTERRUPT are written, next one is INTERRUPT

--- Debug/Core/DRIVERS_V2.0/SPI/SPI_POLLING.d
+++ Debug/Core/DRIVERS_V2.0/SPI/SPI_POLLING.d
@@ -1,2 +1,20 @@
 Core/DRIVERS_V2.0/SPI/SPI_POLLING.o: \
- ../Core/DRIVERS_V2.0/SPI/SPI_POLLING.c
+ ../Core/DRIVERS_V2.0/SPI/SPI_POLLING.c \
+ ../Core/DRIVERS_V2.0/SPI/SPI_POLLING.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/GPIO/GPIO.h
+../Core/DRIVERS_V2.0/SPI/SPI_POLLING.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/GPIO/GPIO.h:
