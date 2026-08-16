f4efbe200faaf5ac9e1b1dbeed342082fd02da31
Bavin <bavinbav5@gmail.com>
2026-08-16 13:51:50 +0300

    i will leave I2C for another time, now i will try to write a driver for SPI perpherial

---
+++ Core/DRIVERS_V2.0/SPI/SPI_POLLING.h
@@ -0,0 +1,8 @@
+#ifndef SPI_POLLING_H
+#define SPI_POLLING_H
+
+#include "stdint.h"
+#include "stm32f446xx.h"
+#include "GPIO.h"
+
+#endif
---
+++ Debug/Core/DRIVERS_V2.0/SPI/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/DRIVERS_V2.0/SPI/SPI_POLLING.c 
+
+OBJS += \
+./Core/DRIVERS_V2.0/SPI/SPI_POLLING.o 
+
+C_DEPS += \
+./Core/DRIVERS_V2.0/SPI/SPI_POLLING.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/DRIVERS_V2.0/SPI/%.o Core/DRIVERS_V2.0/SPI/%.su Core/DRIVERS_V2.0/SPI/%.cyclo: ../Core/DRIVERS_V2.0/SPI/%.c Core/DRIVERS_V2.0/SPI/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/I2C" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/EXTI" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/TIM" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/ADC" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/DMA" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/UART" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/DELAY" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/GPIO" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0" -I"C:/Users/pc/Desktop/STM_PROJECTS/BAREMETAL_DRIVERS_V2.0/Core/DRIVERS_V2.0/CLOCK" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-DRIVERS_V2-2e-0-2f-SPI
+
+clean-Core-2f-DRIVERS_V2-2e-0-2f-SPI:
+	-$(RM) ./Core/DRIVERS_V2.0/SPI/SPI_POLLING.cyclo ./Core/DRIVERS_V2.0/SPI/SPI_POLLING.d ./Core/DRIVERS_V2.0/SPI/SPI_POLLING.o ./Core/DRIVERS_V2.0/SPI/SPI_POLLING.su
+
+.PHONY: clean-Core-2f-DRIVERS_V2-2e-0-2f-SPI
+
