e3ee963f4ce1d15baa861f83d94a42cf57886d6c
Bavin <bavinbav5@gmail.com>
2026-08-17 13:40:26 +0300

    i think the handler is finished to handle 3 modes, transfer, transmit, receive

--- Core/DRIVERS_V2.0/SPI/SPI_INTERRUPT.c
+++ Core/DRIVERS_V2.0/SPI/SPI_INTERRUPT.c
@@ -1,5 +1,22 @@
 #include "SPI_INTERRUPT.h"
 
+typedef struct{
+	uint8_t *rx_buffer;
+	uint8_t *tx_buffer;
+
+	uint16_t rx_length;
+	uint16_t tx_length;
+
+	SPI_TypeDef *spi;
+
+	uint16_t rx_index;
+	uint16_t tx_index;
+
+	uint8_t mode;
+}spi_handle_t;
+
+spi_handle_t spi_data;
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // INLINE FUNCTIONS
@@ -91,6 +108,9 @@ inline static void spi_IT(SPI_TypeDef *spi){
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 void SPIx_INTERRUPT_init(SPI_TypeDef *spi, spi_interrupt_config_t *config){
 	spi_set(spi, config->cs_port, config->cs_pin);
@@ -98,11 +118,87 @@ void SPIx_INTERRUPT_init(SPI_TypeDef *spi, spi_interrupt_config_t *config){
 	spi->CR1&=~(1<<6);
 	spi->CR1&=~((1<<11)|(1<<10)|(1<<7)|(7<<3)|(1<<1)|(1<<0));
 	spi->CR1|=(config->data_format<<11)|(1<<9)|(1<<8)|(config->frame_format<<7)|(config->prescaler<<3)|(1<<2)|(config->clock_polarity<<1)|(config->clock_phase<<0);
-	spi->CR2|=(1<<7)|(1<<6);
+	spi->CR2|=(1<<6);
 	spi->CR2&=~(1<<4);
 	spi_IT(spi);
 	spi->CR1|=(1<<6);
 }
 
+void SPIx_INTERRUPT_transmit(SPI_TypeDef *spi, uint8_t *buffer, uint16_t length){
+	spi_data.mode=0;
+	spi_data.tx_length=length;
+	spi_data.spi=spi;
+	spi_data.tx_index=1;
+
+	spi_data.tx_buffer=buffer+1;
+	spi->CR2|=(1<<7);
+	spi->DR=*buffer++;
+}
 
 
+
+void SPIx_INTERRUPT_receive(SPI_TypeDef *spi, uint8_t *buffer, uint16_t length){
+	spi_data.mode=1;
+	spi_data.rx_buffer=buffer;
+	spi_data.rx_length=length;
+	spi_data.spi=spi;
+	spi_data.rx_index=0;
+	spi->DR=0x00;
+	spi->CR2|=(1<<7);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// END FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// HANDLER FUNCTIONS FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void SPI1_IRQHandler(void){
+	if(spi_data.mode == 0){
+		if(spi_data.spi->SR&(1<<1)){
+			if(spi_data.tx_index<spi_data.tx_length){
+				spi_data.spi->DR=spi_data.tx_buffer[spi_data.tx_index];
+				spi_data.tx_index++;
+			}
+			else spi_data.spi->CR2&=~(1<<7);
+		}
+	}
+
+	if(spi_data.mode == 1){
+		if(spi_data.spi->SR&(1<<1)){
+			if(spi_data.tx_index<spi_data.tx_length){
+				spi_data.spi->DR=0x00;
+				spi_data.tx_index++;
+			}
+			else spi_data.spi->CR2&=~(1<<7);
+		}
+
+		if(spi_data.spi->SR&(1<<0)){
+				spi_data.rx_buffer[spi_data.rx_index]=spi_data.spi->DR;
+				spi_data.rx_index++;
+		}
+	}
+
+	if(spi_data.mode == 2){
+		if(spi_data.spi->SR&(1<<1)){
+			if(spi_data.tx_index<spi_data.tx_length){
+				spi_data.spi->DR=spi_data.tx_buffer[spi_data.tx_index];
+				spi_data.tx_index++;
+			}
+			else spi_data.spi->CR2&=~(1<<7);
+		}
+
+		if(spi_data.spi->SR&(1<<0)){
+				spi_data.rx_buffer[spi_data.rx_index]=spi_data.spi->DR;
+				spi_data.rx_index++;
+		}
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// END HANDLER FUNCTIONS FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////
